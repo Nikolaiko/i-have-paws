b262f9d2bc65cbb2c901553470ed20ca7635c474
Nikolai Baklanov <nikolaiko@mail.ru>
2022-05-02 14:36:45 +0600

    Removed test plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
     }
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
-        classpath ("org.testproject:TestProject:1.0-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.1.3")
         classpath("io.realm.kotlin:gradle-plugin:0.9.0")
